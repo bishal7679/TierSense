dd4c24e2e620aa668a1d60655dcab105ef11b1ce
Deepayan Das <deepayan.das@calsoftinc.com>
2025-07-17 11:02:18 +0530

    filebeat repo gpg key add 1

--- backend/setup_base.sh
+++ backend/setup_base.sh
@@ -12,9 +12,11 @@ if [[ -z "$NFS_SERVER_IP" || -z "$NFS_MOUNT_DIR" ]]; then
   exit 1
 fi
 
+echo "[+] Installing system dependencies..."
+apt update && apt install -y wget gnupg apt-transport-https curl software-properties-common
+
 echo "[+] Adding Elastic GPG key and APT repository..."
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | apt-key add -
-apt-get install -y apt-transport-https
 echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" | tee -a /etc/apt/sources.list.d/elastic-7.x.list
 apt-get update
 
